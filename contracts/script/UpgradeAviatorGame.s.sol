b971b2df64abb1262d44c2359e84d47846a94b5b
Navin Developer <dev@navin.local>
2026-04-05 22:06:23 +0100

    refactor: update UpgradeAviatorGame script

--- contracts/script/UpgradeAviatorGame.s.sol
+++ contracts/script/UpgradeAviatorGame.s.sol
@@ -9,18 +9,24 @@ contract UpgradeAviatorGameScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address proxyAddress = vm.envAddress("AVIATOR_PROXY_ADDRESS");
-        
+
         require(proxyAddress != address(0), "AVIATOR_PROXY_ADDRESS not set");
 
         vm.startBroadcast(deployerPrivateKey);
 
         // 1. Deploy New Implementation
         AviatorGame newImplementation = new AviatorGame();
-        console.log("New Implementation deployed at:", address(newImplementation));
+        console.log(
+            "New Implementation deployed at:",
+            address(newImplementation)
+        );
 
         // 2. Upgrade Proxy
         // call upgradeToAndCall with empty data to perform the upgrade
-        AviatorGame(proxyAddress).upgradeToAndCall(address(newImplementation), "");
+        AviatorGame(payable(proxyAddress)).upgradeToAndCall(
+            address(newImplementation),
+            ""
+        );
         console.log("Proxy upgraded to new implementation");
 
         vm.stopBroadcast();
